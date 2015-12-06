a671d33218098232e15219a826ad16ef3c15cb7b
Mustafa Kamal <mustafa.kamal@ymail.com>
2015-12-06 17:46:06 +0100

    edit minor index dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -94,7 +94,7 @@
                 <div class="page-content">
                     <div id="tab-general">
                         <div id="sum_box" class="row mbl">
-                            <div class="col-sm-6 col-md-3">
+                            <div class="col-sm-6 col-md-4">
                                 <div class="panel profit db mbm">
                                     <div class="panel-body">
                                         <p class="icon">
@@ -113,7 +113,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-6 col-md-3">
+                            <div class="col-sm-6 col-md-4">
                                 <div class="panel income db mbm">
                                     <div class="panel-body">
                                         <p class="icon">
@@ -131,7 +131,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-6 col-md-3">
+                            <div class="col-sm-6 col-md-4">
                                 <div class="panel task db mbm">
                                     <div class="panel-body">
                                         <p class="icon">
@@ -145,24 +145,6 @@
                                             <div role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
                                                 style="width: 50%;" class="progress-bar progress-bar-danger">
                                                 <span class="sr-only">50% Complete (success)</span></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 col-md-3">
-                                <div class="panel visit db mbm">
-                                    <div class="panel-body">
-                                        <p class="icon">
-                                            <i class="icon fa fa-group"></i>
-                                        </p>
-                                        <h4 class="value">
-                                            <span>128</span></h4>
-                                        <p class="description">
-                                            Random number</p>
-                                        <div class="progress progress-sm mbn">
-                                            <div role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"
-                                                style="width: 70%;" class="progress-bar progress-bar-warning">
-                                                <span class="sr-only">70% Complete (success)</span></div>
                                         </div>
                                     </div>
                                 </div>
